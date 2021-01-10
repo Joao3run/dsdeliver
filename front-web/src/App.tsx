ca4fe7305d9687cc806997930645d5e8b1dea31e
Joao3run <57202776+Joao3run@users.noreply.github.com>
2021-01-09 21:06:37 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./componentes/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      Ola
+        <Navbar />
+        <Home />
     </div>
   );
 }

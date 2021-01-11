e64d20207806c058cb5d510390640e9a42ea616a
Joao3run <57202776+Joao3run@users.noreply.github.com>
2021-01-10 22:00:12 -0300

    Fix total

--- backend/src/main/java/br/com/brn/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/br/com/brn/dsdeliver/dto/OrderDTO.java
@@ -24,19 +24,21 @@ public class OrderDTO implements Serializable {
     private Double longitude;
     private Instant moment;
     private OrderStatus status;
+    private Double total;
 
     private List<ProductDTO> producs = new ArrayList<>();
 
     public OrderDTO() {
     }
 
-    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Double total) {
         this.id = id;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
         this.moment = moment;
         this.status = status;
+        this.total = total;
     }
 
     public OrderDTO(Order entity) {
@@ -46,6 +48,7 @@ public class OrderDTO implements Serializable {
         longitude = entity.getLongitude();
         moment = entity.getMoment();
         status = entity.getStatus();
+        total = entity.getTotal();
         producs = entity.getProducts().stream()
                 .map(ProductDTO::new).collect(Collectors.toList());
     }
@@ -100,6 +103,14 @@ public class OrderDTO implements Serializable {
 
     public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
     }
 
     public List<ProductDTO> getProducs() {
